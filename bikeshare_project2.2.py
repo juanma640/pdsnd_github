7d7476a89709d56731f41a2a02506a4458e78cdc
Juan Santos <juanma640@gmail.com>
2020-09-12 23:30:56 +0100

    Improving doc string in function get_filters on bikeshare_project2.2.py

--- bikeshare_project2.2.py
+++ bikeshare_project2.2.py
@@ -12,6 +12,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
+    Interact with the user for the first time running the program
 
     Returns:
         (str) city - name of the city to analyze
